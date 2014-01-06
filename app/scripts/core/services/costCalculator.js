1623e9c667c90d4cc8890743a9b5af3ef748e89c
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-01-06 20:23:21 +0100

    chore: jslint fixes

--- app/scripts/core/services/costCalculator.js
+++ app/scripts/core/services/costCalculator.js
@@ -1,50 +1,64 @@
+/*jslint browser: true */
+
+var angular = window.angular,
+    _ = window._;
+
 angular.module('ut.core.services', ['ut.core.constants'])
-.factory('costCalculator', [
-    'lookups',
-function(lookups) {
+    .factory('costCalculator', [
+        'lookups',
+        function (lookups) {
+            "use strict";
+            
+            var CostCalculator = function (races, professions, items) {
+                this.races = races;
+                this.professions = professions;
+                this.items = items;
+            };
 
-	var CostCalculator = function (races, professions, items) {
-		this.races = races;
-		this.professions = professions;
-		this.items = items;
-	}
+            CostCalculator.prototype.calculatePartyMemberCost = function (member, party) {
+                var self = this,
+                    sameProfMembers,
+                    professionRank,
+                    cost = 0;
 
-	CostCalculator.prototype.calculatePartyMemberCost = function (member, party) {
-		var self = this;
-		
-		var sameProfMembers = _(party.members)
-            .filter(function(x) { return x.profession == member.profession; })
-            .value();
-        
-		var memberRank = _.indexOf(sameProfMembers, member);
+                sameProfMembers = _(party.members)
+                    .filter(function (x) {
+                        return x.profession === member.profession;
+                    })
+                    .value();
 
-		var cost = 0;
-        if (member.race) {
-		  cost += self.races[member.race].cost;
-        }
-        if (member.profession) {
-		  cost += self.professions[member.profession].cost + (memberRank == -1 ? 0 : memberRank) * 10;
-        }
-        
-        if (member.equipment){
-            cost += (_.reduce(member.equipment, function(sum, itemId) {
-                return sum + self.items[itemId].cost;
-            }) || 0);
-        }
+                professionRank = _.indexOf(sameProfMembers, member);
 
-		return cost;
-	};
+                if (member.race) {
+                    cost += self.races[member.race].cost;
+                }
 
-	CostCalculator.prototype.calculatePartyCost = function(party) {
-		var self = this;
-		var costs = _.map(party.members, function(x) {
-			return self.calculatePartyMemberCost(x, party);
-		})
-		return _.reduce(costs, function(result, cost) {
-			return result + cost;
-		});
-	};
+                if (member.profession) {
+                    cost += self.professions[member.profession].cost + (professionRank === -1 ? 0 : professionRank) * 10;
+                }
 
-	return new CostCalculator(lookups.races, lookups.professions, lookups.items);
+                if (member.equipment) {
+                    cost += (_.reduce(member.equipment, function (sum, itemId) {
+                        return sum + self.items[itemId].cost;
+                    }) || 0);
+                }
 
-}]); 
+                return cost;
+            };
+
+            CostCalculator.prototype.calculatePartyCost = function (party) {
+                var self = this,
+                    costs;
+                
+                costs = _.map(party.members, function (x) {
+                    return self.calculatePartyMemberCost(x, party);
+                });
+                
+                return _.reduce(costs, function (result, cost) {
+                    return result + cost;
+                });
+            };
+
+            return new CostCalculator(lookups.races, lookups.professions, lookups.items);
+
+        }]);
