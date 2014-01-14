3c7aec9a52ec499be5d1f97aa1e298a84524b38d
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-01-14 22:29:56 +0100

    fix: visual tweaks

--- app/scripts/controllers/manageParty.js
+++ app/scripts/controllers/manageParty.js
@@ -49,22 +49,33 @@ function ManagePartyController($scope, $routeParams, $window, $location, $modal,
         $scope.notifyInfo('Drużyna zapisana pomyślnie!', 'Wszystkie wprowadzone zmiany zostały zapisane.');
     };
 
-    $scope.removeCharacter = function ($event, character) {
+    $scope.applyCurrentAction = function ($event, character) {
         $event.stopImmediatePropagation();
-        $scope.party.removeCharacter(character);
+
+        if ($scope.state.deleteMode) {
+            $scope.party.removeCharacter(character);
+        }
+
+        if ($scope.state.chiefSelection) {
+            $scope.party.setChief(character);
+        }
+
+        if ($scope.state.mageSelection) {
+            $scope.party.setMage(character);
+        }
     };
-    
+
     $scope.isInSpecialMode = function () {
-      var state = $scope.state;
-      return state.deleteMode ||
-          state.chiefSelection ||
-          state.mageSelection;
+        var state = $scope.state;
+        return state.deleteMode ||
+            state.chiefSelection ||
+            state.mageSelection;
     };
-    
+
     $scope.toggleDeleteMode = function (arg) {
         $scope.state.deleteMode = arg;
     };
-    
+
     $scope.toggleSelectionModeOff = function () {
         $scope.state.chiefSelection = false;
         $scope.state.mageSelection = false;
@@ -73,35 +84,35 @@ function ManagePartyController($scope, $routeParams, $window, $location, $modal,
     $scope.destroyParty = function () {
 
         var DestroyPartyConfirmController = function ($scope, $modalInstance) {
-            
+
             $scope.ok = function () {
                 $modalInstance.close(true);
             };
-            
+
             $scope.cancel = function () {
                 $modalInstance.close(false);
             };
-            
+
         };
-        
-        DestroyPartyConfirmController.$inject = [ '$scope', '$modalInstance' ];
-        
+
+        DestroyPartyConfirmController.$inject = ['$scope', '$modalInstance'];
+
         var confirm = $modal.open({
             templateUrl: 'views/modals/destroyPartyConfirm.html',
             controller: DestroyPartyConfirmController
         });
 
-        confirm.result.then(function(result) {
+        confirm.result.then(function (result) {
             if (!result) {
                 return false;
             }
-            
+
             partyManager.remove($scope.party)
-                    .then(function () {
-                        $scope.updatePartyList();
-                        $location.path('/');
-                        $scope.notifyInfo("Drużyna zniszczona.", "Postacie nie żyją.");
-                    });
+                .then(function () {
+                    $scope.updatePartyList();
+                    $location.path('/');
+                    $scope.notifyInfo("Drużyna zniszczona.", "Postacie nie żyją.");
+                });
         });
     };
 
