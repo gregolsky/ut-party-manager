8366545e9590fdb34ab0e90a83ff8545bf0ed246
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2014-03-01 22:30:05 +0100

    fix: remove redundant party-card-render directive

--- app/scripts/controllers/printParty.js
+++ app/scripts/controllers/printParty.js
@@ -2,7 +2,7 @@ function PrintPartyController($scope, $window) {
     'use strict';
     
     $scope.printParty = function () {
-        var canvas = $window.document.querySelector('[party-card-render] canvas');
+        var canvas = $window.document.querySelector('.partyCardSheet canvas');
         var dataUrl = canvas.toDataURL('image/png');
         $window.open(dataUrl, '', '');
     };
